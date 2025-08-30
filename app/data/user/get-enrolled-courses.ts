f741e508a0dfd9381ef380f4fedfc7e1b8522028
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-08-30 13:03:32 +0800

    user dashboard and enroll course listing

---
+++ app/data/user/get-enrolled-courses.ts
@@ -0,0 +1,40 @@
+import "server-only"
+import { requireUser } from "./require-user";
+import { prisma } from "@/lib/db";
+
+export async function getEnrolledCourses() {
+    const user = await requireUser();
+    const data = await prisma.enrollment.findMany({
+        where: {
+            userId: user.id,
+            status: "ACTIVE",
+        },
+        select: {
+            course:{
+                select: {
+                    id: true,
+                    title: true,
+                    smallDescription: true,
+                    duration: true,
+                    level: true,
+                    status: true,
+                    price:true,
+                    fileKey: true,
+                    slug:true,
+                    category: true,
+                    chapter: {
+                        select: {
+                            id: true,
+                            lessons: {
+                                select: {
+                                    id: true,
+                                }, 
+                            }
+                        }
+                    }
+                }
+            } 
+        }
+    });
+    return data;
+}
--- components/sidebar/nav-main.tsx
+++ components/sidebar/nav-main.tsx
@@ -11,6 +11,9 @@ import {
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
 import Link from "next/link"
+import { usePathname } from "next/navigation"
+import { cn } from "@/lib/utils"
+import path from "path"
 
 export function NavMain({
   items,
@@ -21,29 +24,36 @@ export function NavMain({
     icon?: Icon
   }[]
 }) {
+
+
+  const pathname = usePathname()
   return (
     <SidebarGroup>
       <SidebarGroupContent className="flex flex-col gap-2">
         <SidebarMenu>
-          <SidebarMenuItem className="flex items-center gap-2">
-            <SidebarMenuButton
-              asChild
-              tooltip="Quick Create"
-              className="bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
-            >
-              <Link href="/admin/courses/create">
-                <IconCirclePlusFilled />
-                <span>Quick Create</span> 
-              </Link>
-            </SidebarMenuButton>
-          </SidebarMenuItem>
+          {
+            (pathname.startsWith("/admin")&& (
+                <SidebarMenuItem className="flex items-center gap-2">
+                  <SidebarMenuButton
+                    asChild
+                    tooltip="Quick Create"
+                    className="bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
+                  >
+                    <Link href="/admin/courses/create">
+                      <IconCirclePlusFilled />
+                      <span>Quick Create</span> 
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+            ))
+          }
         </SidebarMenu>
         <SidebarMenu>
           {items.map((item) => (
             <SidebarMenuItem key={item.title}>
               <SidebarMenuButton asChild tooltip={item.title}>
-                <Link href={item.url}>
-                  {item.icon && <item.icon />}
+                <Link href={item.url} className={cn(pathname === item.url && "bg-accent text-accent-foreground")}>
+                  {item.icon && <item.icon className={cn(pathname === item.url && "text-primary")} />}
                   <span>{item.title}</span>
                 </Link>
               </SidebarMenuButton>
