6c26f935f04a898abe32ed9a5cdbd654fec8fb8f
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-08-31 16:55:57 +0800

    update text editor

--- components/rich-text-editor/Editor.tsx
+++ components/rich-text-editor/Editor.tsx
@@ -5,43 +5,39 @@ import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Menubar from "./Menubar";
 import TextAlign from "@tiptap/extension-text-align";
-import { ControllerRenderProps, FieldValues } from "react-hook-form";
+import type { ControllerRenderProps } from "react-hook-form";
 
 interface RichTextEditorProps {
-    field: ControllerRenderProps<FieldValues, string>;
+  field: ControllerRenderProps<any, string>; // type-safe field from RHF
 }
 
 const RichTextEditor = ({ field }: RichTextEditorProps) => {
-    const editor = useEditor({
-        extensions: [
-        StarterKit,
-        TextAlign.configure({
-            types: ["heading", "paragraph"],
-        }),
-        ],
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      TextAlign.configure({
+        types: ["heading", "paragraph"],
+      }),
+    ],
+    editorProps: {
+      attributes: {
+        class:
+          "min-h-[300px] p-4 focus:outline-none prose prose-sm sm:prose lg:prose-lg xl:prose-xl dark:prose-invert !w-full !max-w-none",
+      },
+    },
+    immediatelyRender: false,
+    onUpdate: ({ editor }) => {
+      field.onChange(JSON.stringify(editor.getJSON()));
+    },
+    content: field.value ? JSON.parse(field.value) : null,
+  });
 
-        editorProps: {
-        attributes: {
-            class:
-            "min-h-[300px] p-4 focus:outline-none prose prose-sm sm:prose lg:prose-lg xl:prose-xl dark:prose-invert !w-full !max-w-none",
-        },
-        },
-
-        immediatelyRender: false,
-
-        onUpdate: ({ editor }) => {
-        field.onChange(JSON.stringify(editor.getJSON()));
-        },
-
-        content: field.value ? JSON.parse(field.value) : null,
-    });
-
-    return (
-        <div className="w-full border border-input rounded-lg overflow-hidden dark:bg-input/30">
-        <Menubar editor={editor} />
-        <EditorContent editor={editor} />
-        </div>
-    );
+  return (
+    <div className="w-full border border-input rounded-lg overflow-hidden dark:bg-input/30">
+      <Menubar editor={editor} />
+      <EditorContent editor={editor} />
+    </div>
+  );
 };
 
 export default RichTextEditor;
